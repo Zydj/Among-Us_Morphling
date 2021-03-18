7d36f33cca30453e9f0348e0918a6aac439e8f9d
Zydj <charles.desjardins96@gmail.com>
2021-03-18 15:58:29 -0400

    Initial commit

---
+++ Morphling/Morphling/Morphling.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using Reactor;
+
+namespace Morphling
+{
+    [BepInPlugin(Id)]
+    [BepInProcess("Among Us.exe")]
+    [BepInDependency(ReactorPlugin.Id)]
+    public class Morphling : BasePlugin
+    {
+        public static bool debug = false;
+
+        public const string Id = "org.bepinex.plugins.Morphling";
+
+        public static BepInEx.Logging.ManualLogSource log;
+
+        public Harmony Harmony { get; } = new Harmony(Id);
+
+        public ConfigEntry<string> Name { get; private set; }
+
+        public override void Load()
+        {
+            log = Log;
+            log.LogMessage("Morphling Mod Loaded");           
+
+            Harmony.PatchAll();
+        }        
+    }
+}
