aacea0753226a442309728d8af24f1f7c9995c37
Zydj <charles.desjardins96@gmail.com>
2021-03-18 22:22:49 -0400

    Created the patch for the morphling settings in the sidebar

---
+++ Morphling/Morphling/GameOptionsDataPatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+
+namespace Morphling
+{
+    [HarmonyPatch]
+    public class GameOptionsDataPatch
+    {
+        [HarmonyPatch(typeof(GameOptionsData), nameof(GameOptionsData.Method_5))]
+        public static void Postfix(ref string __result)
+        {
+            if (Morphling.morphlingEnabled)
+            {
+                __result += "Morphling Role: On\n";
+            }
+            else
+            {
+                __result += "Morphling Role: Off\n";
+            }
+
+            __result += "Morphling Morph Duration: " + Morphling.morphDuration.ToString() + "s\n";
+
+            __result += "Morphling Smaple Cooldown: " + Morphling.sampleCooldown.ToString() + "s\n";
+
+            HudManager.Instance.GameSettings.scale = 0.6f;
+        }
+    }
+}
