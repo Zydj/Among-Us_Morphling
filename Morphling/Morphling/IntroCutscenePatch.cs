73b7c0a47581323797309b4c6a9cba592899058e
Zydj <charles.desjardins96@gmail.com>
2021-03-18 23:46:02 -0400

    Added the patch to show the morphling

---
+++ Morphling/Morphling/IntroCutscenePatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace Morphling
+{
+    [HarmonyPatch]
+    public static class IntroCutscenePatch
+    {
+        [HarmonyPatch(typeof(IntroCutscene.Nested_0), nameof(IntroCutscene.Nested_0.MoveNext))]
+        public static void Postfix(IntroCutscene.Nested_0 __instance)
+        {
+            if (!Morphling.morphlingEnabled)
+            {
+                return;
+            }
+
+            if (PlayerController.getLocalPlayer().hasComponent("Morphling"))
+            {
+                __instance.__this.Title.Text = "Morphling";
+            }
+        }
+    }
+}
