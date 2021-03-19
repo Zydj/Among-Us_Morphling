723a12c27877f771ce0a4522901f8f3a23429eb4
Zydj <charles.desjardins96@gmail.com>
2021-03-19 01:06:07 -0400

    Added the code to show the future morph button
    Added the code to show the morph button on the screen. For now, it doesn't have the correct icon but it will be set later

---
+++ Morphling/Morphling/HudManagerPatch.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using InnerNet;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Morphling
+{
+    [HarmonyPatch]
+    public static class HudManagerPatch
+    {
+        [HarmonyPatch(typeof(HudManager), nameof(HudManager.Update))]
+        static void Postfix(HudManager __instance)
+        {
+            if (!Morphling.morphlingEnabled)
+            {
+                return;
+            }
+
+            if (AmongUsClient.Instance.GameState != InnerNetClient.Nested_0.Started)
+                return;
+
+            if (!Morphling.introDone)
+            {
+                return;
+            }
+
+            if (PlayerController.LocalPlayer.hasComponent("Morphling") && !PlayerController.LocalPlayer.playerdata.Data.AKOHOAJIHBE)
+            {
+                KillButtonManager morphButton = GameObject.FindObjectsOfType<KillButtonManager>().ToList().Where(x => x.name == "Morph").FirstOrDefault();
+
+                if (morphButton != null)
+                {
+                    morphButton.gameObject.SetActive(false);
+                    morphButton.isActive = false;
+
+                    GameObject.Destroy(morphButton);
+
+                    morphButton = null;
+                }
+
+                morphButton = GameObject.Instantiate(__instance.KillButton);
+                morphButton.transform.position = __instance.KillButton.transform.position + new Vector3(0, 1.25f, 0);                                   
+
+                morphButton.gameObject.SetActive(true);
+                morphButton.isActive = true;
+
+                morphButton.name = "Morph";                          
+            }
+        }
+    }
+}
