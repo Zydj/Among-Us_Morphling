6eaedf45d5b810c4e85ea0f15ea38bb0b6a86ffd
Zydj <charles.desjardins96@gmail.com>
2021-03-18 22:25:49 -0400

    Added the patch for the syncing of the rpc settings and the handling of that RPC

---
+++ Morphling/Morphling/PlayerControlPatch.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using Hazel;
+using System.Linq;
+
+namespace Morphling
+{
+    [HarmonyPatch]
+    class PlayerControlPatch
+    {
+        [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.HandleRpc))]
+        static void Postfix(byte ACCJCEHMKLN, MessageReader HFPCBBHJIPJ)
+        {
+            if (Morphling.debug)
+            {
+                Morphling.log.LogMessage("RPC is:" + ACCJCEHMKLN);
+            }
+
+            switch (ACCJCEHMKLN)
+            {
+                case (byte)CustomRPC.SyncCustomSettingsMorphling:
+                    {
+                        Morphling.morphlingEnabled = HFPCBBHJIPJ.ReadBoolean();
+                        Morphling.morphDuration = System.BitConverter.ToSingle(HFPCBBHJIPJ.ReadBytes(4).ToArray(), 0);
+                        Morphling.sampleCooldown = System.BitConverter.ToSingle(HFPCBBHJIPJ.ReadBytes(4).ToArray(), 0);
+                        break;
+                    }
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.RpcSyncSettings))]
+        public static void Postfix()
+        {
+            if (PlayerControl.AllPlayerControls.Count > 1)
+            {
+                MessageWriter writer = AmongUsClient.Instance.StartRpc(PlayerControl.LocalPlayer.NetId, (byte)CustomRPC.SyncCustomSettingsMorphling, Hazel.SendOption.Reliable);
+                writer.Write(Morphling.morphlingEnabled);
+                writer.Write(Morphling.morphDuration);
+                writer.Write(Morphling.sampleCooldown);
+                writer.EndMessage();
+            }
+        }
+    }
+}
