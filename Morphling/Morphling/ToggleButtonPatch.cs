b1a66d82cdb8dd9b04258125bb5804cf4b6df524
Zydj <charles.desjardins96@gmail.com>
2021-03-18 22:24:50 -0400

    Added the patch to handle the toggle options for the morphling in the menu

---
+++ Morphling/Morphling/ToggleButtonPatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+
+namespace Morphling
+{
+    [HarmonyPatch]
+    class ToggleButtonPatch
+    {
+        [HarmonyPatch(typeof(ToggleOption), nameof(ToggleOption.Toggle))]
+        public static bool Prefix(ToggleOption __instance)
+        {
+            if (__instance.TitleText.Text == "Morphling Role")
+            {
+                Morphling.morphlingEnabled = !Morphling.morphlingEnabled;
+                PlayerControl.LocalPlayer.RpcSyncSettings(PlayerControl.GameOptions);
+
+                __instance.oldValue = Morphling.morphlingEnabled;
+                __instance.CheckMark.enabled = Morphling.morphlingEnabled;
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
