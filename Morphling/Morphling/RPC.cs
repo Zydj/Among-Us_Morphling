13723788e8ad41d3fcbf97d07d285343264101fb
Zydj <charles.desjardins96@gmail.com>
2021-03-18 22:26:09 -0400

    Added the enum for the custom RPCs

---
+++ Morphling/Morphling/RPC.cs
@@ -0,0 +1,18 @@
+﻿namespace Morphling
+{    
+    enum CustomRPC
+    {
+        SetJester = 40,
+        JesterWin = 50,
+        SetLocalPlayersJester = 41,
+        SyncCustomSettingsJester = 42,
+        SetLastPlayerTask = 43,
+        SetSheriff = 44,
+        SetLocalPlayersSheriff = 45,
+        SheriffKill = 46,
+        SyncCustomSettingsSheriff = 47,
+        SetLocalPlayersMorphling = 48,
+        SetMorphling = 49,
+        SyncCustomSettingsMorphling = 51
+    }
+}
